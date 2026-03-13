5863a83162b0e794aa5942de3d1f03ecbb63cbe2
thonocodesiu <thongtg7@gmail.com>
2026-03-13 14:24:50 +0700

    first commit

---
+++ controllers/users.js
@@ -0,0 +1,66 @@
+let userModel = require('../schemas/users');
+
+module.exports = {
+    CreateAnUser: function (username, password,
+        email, role, fullname, avatar, status, logincount) {
+        return new userModel(
+            {
+                username: username,
+                password: password,
+                email: email,
+                fullName: fullname,
+                avatarUrl: avatar,
+                status: status,
+                role: role,
+                loginCount: logincount
+            }
+        )
+    },
+    FindByUsername: async function (username) {
+        return await userModel.findOne({
+            username: username,
+            isDeleted: false
+        })
+    },
+    FailLogin: async function (user) {
+        user.loginCount++;
+        if (user.loginCount == 3) {
+            user.loginCount = 0;
+            user.lockTime = new Date(Date.now() + 60 * 60 * 1000)
+        }
+        await user.save()
+    },
+    SuccessLogin: async function (user) {
+        user.loginCount = 0;
+        await user.save()
+    },
+    GetAllUser: async function () {
+        return await userModel
+            .find({ isDeleted: false }).populate({
+                path: 'role',
+                select: 'name'
+            })
+    },
+    FindById: async function (id) {
+        try {
+            let getUser = await userModel
+                .findOne({ isDeleted: false, _id: id }).populate({
+                    path: 'role',
+                    select: 'name'
+                })
+            return getUser;
+        } catch (error) {
+            return false
+        }
+    },
+    ChangePass: async function (user, newPass) {
+        try {
+            console.log(user, newPass)
+            user.password = newPass
+            await user.save()
+            return true
+        } catch (error) {
+            return false
+        }
+    }
+}
---
+++ routes/auth.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = express.Router();
+let userController = require('../controllers/users')
+let { RegisterValidator, handleResultValidator, changePassValidator } = require('../utils/validatorHandler')
+let bcrypt = require('bcrypt')
+let jwt = require('jsonwebtoken')
+let { checkLogin } = require('../utils/authHandler')
+/* GET home page. */
+router.post('/register', RegisterValidator, handleResultValidator, async function (req, res, next) {
+    let newUser = userController.CreateAnUser(
+        req.body.username,
+        req.body.password,
+        req.body.email,
+        "69aa8360450df994c1ce6c4c"
+    );
+    await newUser.save()
+    res.send({
+        message: "dang ki thanh cong"
+    })
+});
+router.post('/login', async function (req, res, next) {
+    let { username, password } = req.body;
+    let getUser = await userController.FindByUsername(username);
+    if (!getUser) {
+        res.status(403).send("tai khoan khong ton tai")
+    } else {
+        if (getUser.lockTime && getUser.lockTime > Date.now()) {
+            res.status(403).send("tai khoan dang bi ban");
+            return;
+        }
+        if (bcrypt.compareSync(password, getUser.password)) {
+            await userController.SuccessLogin(getUser);
+            let token = jwt.sign({
+                id: getUser._id
+            }, "secret", {
+                expiresIn: '30d'
+            })
+            res.send(token)
+        } else {
+            await userController.FailLogin(getUser);
+            res.status(403).send("thong tin dang nhap khong dung")
+        }
+    }
+
+});
+router.get('/me', checkLogin, function (req, res, next) {
+    res.send(req.user)
+})
+router.put('/change-pass', changePassValidator, checkLogin, handleResultValidator, async function (req, res, next) {
+    let { newPass } = req.body;
+    if (bcrypt.compareSync(newPass, req.user.password)) {
+        res.status(403).send("mat khau moi khong duoc trung voi mat khau cu")
+        return;
+    } else {
+        let result = await userController.ChangePass(req.user, newPass)
+        if (!result)
+            res.status(500).send("loi he thong")
+        else
+            res.send("thay doi mat khau thanh cong")
+    }
+})
+
+module.exports = router;
